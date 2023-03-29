dcad9bb740fcaa64e0b0a626f9cc83230467a6ab
chajuhui123 <chajuhui123@naver.com>
2023-03-29 23:16:39 +0900

    [feat] LDAP 시작 로직 fast API 포맷으로 변경

--- back/test.py
+++ back/test.py
@@ -1,32 +1,35 @@
 import ldap
+import config
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-def authenticate(ldap_object, ldap_address, user_name, password):
+
+class UserInfo(BaseModel):
+    user_name: str
+    password: str
+
+
+app = FastAPI()
+
+def initialize_ldap(ldap_address):
+    ldap_object = ldap.initialize(ldap_address)
+    return ldap_object
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/sign-in")
+def authenticate_ldap(userInfo : UserInfo):
     try:
-        print(ldap_object.simple_bind_s(user_name, password))
+        ldap_object = initialize_ldap(config.LDAP_SERVER)
+        print(ldap_object.simple_bind_s(userInfo.user_name, userInfo.password))
     except ldap.INVALID_CREDENTIALS:
        ldap_object.unbind()
-       return 'Wrong username ili password'
+       return {"res": "Wrong username ili password"}
     except Exception as e:
         print(e)
         return "error"
 
-    return "Succesfully authenticated"
-
-
-def initialize_ldap(ldap_address):
-    ldap_object = ldap.initialize(ldap_address)
-    
-    return ldap_object
-
-
-def main():
-    ldap_address = "ldap://" # {address}
-    user_name = "insert your email"
-    password = "insert your password"
-
-    ldap_object = initialize_ldap(ldap_address)
-    print(authenticate(ldap_object, ldap_address, user_name, password))
-
-
-if __name__ == '__main__':
-    main()
+    return {"res": "Succesfully authenticated"}
