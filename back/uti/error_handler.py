a5737a8cce9389d8ee1648afc10121361d52ce92
chajuhui123 <chajuhui123@naver.com>
2023-03-31 23:20:08 +0900

    [feat] error handler 공통화

---
+++ back/uti/error_handler.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.exception_handlers import (
+    http_exception_handler,
+    request_validation_exception_handler,
+)
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+from test import app
+
+@app.exception_handler(HTTPException)
+async def custom_http_exception_handler(request, exc):
+    print(f"OMG! An HTTP error!: {repr(exc)}")
+    return await http_exception_handler(request, exc)
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    print(f"OMG! The client sent invalid data!: {exc}")
+    return await request_validation_exception_handler(request, exc)
+
